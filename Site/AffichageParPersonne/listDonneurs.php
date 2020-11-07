6bf09135be3e78d671ed13bd7e631c1db74f553b
SuzanFrogg <33319686+SuzanFrogg@users.noreply.github.com>
2020-11-07 17:55:21 +0100

    Affichage Par personne

---
+++ Site/AffichageParPersonne/listDonneurs.php
@@ -0,0 +1,43 @@
+<?php
+
+//Connexion bd
+$db = new PDO('mysql:host=localhost; dbname=PtutS3', 'root', '',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+//Création des variables
+$idsDonneurs = [];
+$nomsDonneurs = [];
+$fonctionsDonneurs = [];
+
+//Requête
+$req = $db->query('SELECT idPersonne as idD, nom as nomD, fonction as fonctionD FROM personne
+                                        INNER JOIN don on personne.idPersonne = don.idAuteur
+                                        GROUP BY personne.idPersonne');
+while ($row= $req->fetch())
+{
+    array_push($idsDonneurs,$row['idD']);
+    array_push($nomsDonneurs,$row['nomD']);
+    array_push($fonctionsDonneurs,$row['fonctionD']);
+
+}
+
+?>
+
+
+<!--HTML-->
+<!DOCTYPE html>
+<html lang="fr">
+    <body>
+    <h1>Liste des Donnateurs</h1>
+    <p>
+    <?php for ($i = 0; $i < count($idsDonneurs) ; $i++) { //Afficher tous les donneurs
+
+        echo ' <a href="donPerDonnateur.php?id='. $idsDonneurs[$i] .'">
+                ' .$nomsDonneurs[$i] . '  ' . $fonctionsDonneurs[$i] . ' </a> <br/>
+
+                    ';
+
+    }  ?>
+    </p>
+    </body>
+
+</html>
