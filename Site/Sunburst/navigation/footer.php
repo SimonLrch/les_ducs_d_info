2d2ef0a8726572e843c335c47fa463f43b53d885
SimonLrch <45842657+SimonLrch@users.noreply.github.com>
2020-11-15 23:05:53 +0100

    Ajout du sunburst
    Affichage de variable manquant dans le sunburst, test de la base de donnée en affichant une variable sur une page html check

---
+++ Site/Sunburst/navigation/footer.php
@@ -0,0 +1,12 @@
+<div id="footer">
+    <div class="bottom-links">
+        <div class="left">
+            <div class="sync-text">
+                Copyright &copy; 2020 IUT Dijon.
+            </div>
+        </div>
+        <div class="right">
+          
+        </div>
+    </div>
+</div>
