5302e5895a52318ece2ae0b0bc7dd79a35406a57
Oniris <72936212+Oniris@users.noreply.github.com>
2020-11-30 14:30:52 +0100

    Update DonGeographique.php

--- Site/AffichageGeographique/DonGeographique.php
+++ Site/AffichageGeographique/DonGeographique.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 	<?php
-		//Connexion bd
+		// Conexion à la base de données
 		require_once("../include/dbConfig.php");
 		$db = getPDO("PtutS3");
 		$db_villes = getPDO("PtutS3_villes");
-		
+	
 		//Création des variables
 		$id = [];
 		$nomVille = [];
@@ -49,6 +49,7 @@
 		{
 			array_push($longi,$row['longitude']);
 		}
+		
 		//Si on veut afficher les villes non trouvés
 		/*for($i = 0; $i < count($nomVille); $i++){
 			for ($j = 0; $j < count($sourceVille); $j++) {
@@ -65,11 +66,10 @@
 		for($i = 0; $i < count($nomVille); $i++){
 			for ($j = 0; $j < count($sourceVille); $j++) {
 				if($nomVille[$i] == $sourceVille[$j]){
-					array_push($villes, array($nomVille[$i] => array("lat" => $lat[$j], "lon" => $longi[$j])));
+					array_push($villes, array("ville" => $nomVille[$i], "lat" => $lat[$j], "lon" => $longi[$j]));
 				}
 			}
-		}
-		$ville = json_encode($villes);
+		}		
 	?>
     <head>
         <meta charset="utf-8">
@@ -83,8 +83,8 @@
 	        height:400px;
 	    }
 		</style>
-	</style>
-	<title>Géographique</title>
+		</style>
+		<title>Géographique</title>
     </head>
     <body>
 		<?php include'../include/mainHeader.php' ?>
@@ -97,56 +97,33 @@
         <!-- Fichiers Javascript -->
         <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
         <script type='text/javascript' src='https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster.js'></script>
-	<script>
-		var id = <?php echo json_encode($id); ?>;
-		var villes = <?php echo json_encode($villes); ?>;
-	</script>
-	<script type="text/javascript">
-		console.log(villes);
-	    // On initialise la latitude et la longitude de Paris (centre de la carte)
-	    var lat = 48.852969;
-	    var lon = 2.349903;
-	    var macarte = null;
-        var markerClusters; // Servira à stocker les groupes de marqueurs
-		
-	    // Fonction d'initialisation de la carte
-        function initMap() {
-			var markers = []; // Nous initialisons la liste des marqueurs
-			// Nous définissons le dossier qui contiendra les marqueurs
-			var iconBase = 'marker.png';
-			// Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
-			macarte = L.map('map').setView([lat, lon], 11);
-			markerClusters = L.markerClusterGroup(); // Nous initialisons les groupes de marqueurs
-			// Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
-			L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-				// Il est toujours bien de laisser le lien vers la source des données
-				attribution: 'données © OpenStreetMap/ODbL - rendu OSM France',
-				minZoom: 1,
-				maxZoom: 20
-			}).addTo(macarte);
-			// Nous parcourons la liste des villes
-			for (ville in villes) {
-				// Nous définissons l'icône à utiliser pour le marqueur, sa taille affichée (iconSize), sa position (iconAnchor) et le décalage de son ancrage (popupAnchor)
-				var myIcon = L.icon({
-					iconUrl: iconBase,
-					iconSize: [50, 50],
-					iconAnchor: [25, 50],
-					popupAnchor: [-3, -76],
-				});
-				var marker = L.marker([villes[ville].lat, villes[ville].lon], { icon: myIcon }); // pas de addTo(macarte), l'affichage sera géré par la bibliothèque des clusters		
-				var id = document.getElementById("idDon").innerHTML;
-				marker.bindPopup(ville + " " + id[0]);
-				markers.push(marker); // Nous ajoutons le marqueur à la liste des marqueurs
-				markerClusters.addLayer(marker); // Nous ajoutons le marqueur aux groupes
+		<script>
+			var id = <?php echo json_encode($id); ?>;
+			var villes = <?php echo json_encode($villes); ?>;
+		</script>
+		<script type="text/javascript">
+			// On initialise la latitude et la longitude de Paris (centre de la carte)
+			var lat = 48.852969;
+			var lon = 2.349903;
+			var macarte = null;
+			
+			// Fonction d'initialisation de la carte
+			function initMap() {
+				var map = L.map('map').setView([lat, lon], 5);
+
+				L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+					attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				}).addTo(map);
+
+				for (ville in villes) {
+					L.marker([villes[ville].lat, villes[ville].lon]).addTo(map)
+						.bindPopup(villes[ville].ville);
+				}
 			}
-			var group = new L.featureGroup(markers); // Nous créons le groupe des marqueurs pour adapter le zoom
-			macarte.fitBounds(group.getBounds().pad(0.5)); // Nous demandons à ce que tous les marqueurs soient visibles, et ajoutons un padding (pad(0.5)) pour que les marqueurs ne soient pas coupés
-			macarte.addLayer(markerClusters);
-		}
-	    window.onload = function(){
-		// Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
-		initMap(); 
-	    };
-	</script>
+			window.onload = function(){
+			// Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
+			initMap(); 
+			};
+		</script>
     </body>
 </html>
