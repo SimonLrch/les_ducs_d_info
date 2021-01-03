17a2fb1904832128349c661389d91c192feb546c
Oniris <72936212+Oniris@users.noreply.github.com>
2021-01-03 18:00:28 +0100

    Update mainHeader.php
    le header et le footer marchait plus j'ai remis bien.

--- Site/include/mainHeader.php
+++ Site/include/mainHeader.php
@@ -5,15 +5,15 @@ if (session_status() == PHP_SESSION_NONE) {
 require_once(realpath(__DIR__ . '/..') . "/configRoot.php");
 ?>
 <header class="topbar">
-	<a href="#"><img class="topbar-logo" src="<?php echo ROOT_PATH?>Images/logo.png" alt="Logo Illumination"/></a>
+	<a href="#"><img class="topbar-logo" src="<?php echo ROOT_PATH?>../Images/logo.png" alt="Logo Illumination"/></a>
 	<nav class="topbar-menu">
-		<a href="<?php echo ROOT_PATH?>index.php">Accueil</a>
-		<a href="<?php echo ROOT_PATH?>restitution.php">Restitution</a>
-		<a href="<?php echo ROOT_PATH?>donation-submission.php">Ajout</a>
+		<a href="<?php echo ROOT_PATH?>../index.php">Accueil</a>
+		<a href="<?php echo ROOT_PATH?>../restitution.php">Restitution</a>
+		<a href="<?php echo ROOT_PATH?>../donation-submission.php">Ajout</a>
 		<?php if (isset($_SESSION["email"])): ?>
-		<a href="<?php echo ROOT_PATH?>profil/profil.php">Profil</a>
+		<a href="<?php echo ROOT_PATH?>../profil/profil.php">Profil</a>
 		<?php else: ?>
-		<a href="<?php echo ROOT_PATH?>connexion.php">Connexion</a>
+		<a href="<?php echo ROOT_PATH?>../connexion.php">Connexion</a>
 		<?php endif; ?>
 	</nav>
 </header>
