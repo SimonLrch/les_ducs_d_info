aebf3e7037577ff5d1e6b73e10bd76bc9df39e34
Oniris <72936212+Oniris@users.noreply.github.com>
2021-01-08 15:21:06 +0100

    sunburst aspect visuel

--- Site/AffichageSunburst/navigation/footer.php
+++
@@ -1,9 +0,0 @@
-<div id="footer">
-    <div class="footer">
-        <div class="left">
-            <div class="text">
-                Projet Tutoré S3 Les ducs d'info
-            </div>
-        </div>
-    </div>
-</div>
