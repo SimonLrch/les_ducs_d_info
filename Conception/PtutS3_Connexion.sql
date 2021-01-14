8327fe8bdbe6e360696151bfe4e87d8a976b2a31
SuzanFrogg <33319686+SuzanFrogg@users.noreply.github.com>
2021-01-14 15:01:11 +0100

    Update PtutS3_Connexion.sql

--- Conception/PtutS3_Connexion.sql
+++ Conception/PtutS3_Connexion.sql
@@ -21,3 +21,11 @@
     Nom varchar(50) not null,
     Prenom varchar(50) not null,
     TypeCompte varchar(50) not null);
+
+--
+-- Contenu de la table `compte`
+--
+
+INSERT INTO `compte` (`IdCompte`, `Email`, `HashPassword`, `Nom`, `Prenom`, `TypeCompte`) VALUES
+(1, 'admin@example.com', '$2y$10$szLhFty5C87V8ACSScacUuH.gNWNd3Xw1s/gTVhJ5SD2oV7oS8wZ2', 'Admin', 'Admin', 'Administrateur'),
+(2, 'user@example.com', '$2y$10$l05OoNRlUeNDSvSHSDID0uwzB3cxufKSLWZqZWHC9/WqEAC5.fofi', 'User', 'User', 'Utilisateur');
