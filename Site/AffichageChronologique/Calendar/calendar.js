3462bbc635907443509d6c99c612d4d6d5861b56
Thibault <55053185+Spaicat@users.noreply.github.com>
2021-01-14 19:39:39 +0100

    Commentaires de calendar.js

--- Site/AffichageChronologique/Calendar/calendar.js
+++ Site/AffichageChronologique/Calendar/calendar.js
@@ -1,6 +1,7 @@
 function Calendar(element, initDate) {
-    this.date = (initDate==null) ? new Date() : initDate;
+    this.date = (initDate==null) ? new Date(1400, 0) : initDate;
 
+    //date a afficher
     this.day = this.date.getDate();
     this.month = this.date.getMonth();
     this.year = this.date.getFullYear();
@@ -59,6 +60,7 @@ function Calendar(element, initDate) {
     this.calendarGridMonths.id = "calendar-grid-months";
     this.calendarDetailsContainer.id = "calendar-date-details";
 
+    //On initialise tout
     this.initCalendar();
     this.initHeader();
     this.initGrid();
@@ -66,20 +68,33 @@ function Calendar(element, initDate) {
     this.docElement.appendChild(this.calendarMainContainer);
 }
 
+/**
+ * Initialise le calendrier (evenements)
+ */
 Calendar.prototype.initCalendar = function() {
-    let objCalendar = this;
-    this.calendarGridMonths.addEventListener("click", function(event) {
+    //Evénement de lorsqu'on clique sur un mois
+    this.calendarGridMonths.addEventListener("click", (event) => {
         let item = event.target;
-        if (item.nodeName == objCalendar.calendarGridMonths.children[0].children[1].children[0].nodeName && !item.classList.contains("calendar-date-null")) {
+
+        //On vérifie si l'élément qu'on a séléctionné est bien une date (un jour) et que ce n'est pas une date vide (pour l'esthétisme)
+        if (item.nodeName == this.calendarGridMonths.children[0].children[1].children[0].nodeName && !item.classList.contains("calendar-date-null")) {
+            //On enlève la selection sur l'élément qui était précedemment selectionné
             Array.from(document.querySelectorAll(".calendar-selected")).forEach(element => element.classList.remove("calendar-selected"));
+
             item.classList.add("calendar-selected");
+
+            //On prends la date de l'élément
             let dateTimeHtml = item.dateTime.split("-");
-            objCalendar.dateSelected = new Date(dateTimeHtml[0], parseInt(dateTimeHtml[1])-1, dateTimeHtml[2]);
-            objCalendar.getInfoDate(objCalendar.dateSelected);
+            this.dateSelected = new Date(dateTimeHtml[0], parseInt(dateTimeHtml[1])-1, dateTimeHtml[2]);
+
+            this.getInfoDate(this.dateSelected);
         }
     });
 }
 
+/**
+ * On initialise l'en-tête (la barre avec l'année)
+ */
 Calendar.prototype.initHeader = function() {
     //On s'assure que l'élément principal est vide
     this.calendarHeaderContainer.innerHTML = "";
@@ -123,6 +138,12 @@ Calendar.prototype.initGrid = function() {
     this.calendarMainContainer.appendChild(this.calendarGridMonths);
 }
 
+/**
+ * Renvoie l'affichage d'un mois
+ * @param {int} month Mois que l'on veut récupérer
+ * @param {int} year Année du mois que l'on veut récupérer
+ * @return l'affichage du mois
+ */
 Calendar.prototype.drawMonth = function(month, year) {
     let daysToShow = this.getDaysOfMonth(month, year);
 
@@ -136,14 +157,19 @@ Calendar.prototype.drawMonth = function(month, year) {
 
     let calendarGridFinal = [];
     
+    //On récupère tous les jours à afficher
     daysToShow.forEach(day => {
         let dayElt = document.createElement("time");
+
+        //Si le jours correspond bien à un jour (pas les espaces devant le premier jours)
         if (day != null) {
-            if (this.dateSelected != null) {
-                if (this.dateSelected.toString() == day.toString()) {
-                    dayElt.classList.add("calendar-selected");
-                }
+            //S'il y a une date de séléctionné et que c'est la date du jours (du foreach)
+            if (this.dateSelected != null && this.dateSelected.toString() == day.toString()) {
+                //On ajoute la classe qui indique si c'est séléctionné à la date courante (du foreach)
+                dayElt.classList.add("calendar-selected");
             }
+
+            //On ajoute la date au contenu et en attribut (attribut de la balise <time>)
             let dayString = day.toString().split(" ")[2];
             dayElt.innerText = dayString;
             dayElt.setAttribute("datetime", this.getDateText(new Date(year, month, dayString)));
@@ -173,8 +199,11 @@ Calendar.prototype.getDaysOfMonth = function(month, year) {
     return listOfDays;
 }
 
+/**
+ * Donne la date sous forme de texte, rajoute des 0 et des tirets (ex -> "08-01-1400")
+ * @param {Date} date date à récupérer
+ */
 Calendar.prototype.getDateText = function (date) {
-    // Setting current date as readable text.
     let dayAsText = (date.getDate()<10) ? "0"+date.getDate() : ""+date.getDate();
     let monthAsText = ((date.getMonth()+1)<10) ? "0"+(date.getMonth()+1) : ""+(date.getMonth()+1);
     let yearAsText = ""+date.getFullYear();
@@ -182,14 +211,21 @@ Calendar.prototype.getDateText = function (date) {
     return yearAsText + "-" + monthAsText + "-" + dayAsText;
 }
 
+/**
+ * Met à jour le graphique (pas les évenements)
+ */
 Calendar.prototype.update = function() {
-    let objCalendar = this;
-    window.requestAnimationFrame(function() {
-        objCalendar.calendarTitle.innerText = "Année " + objCalendar.year;
-        objCalendar.initGrid();
+    window.requestAnimationFrame(() => {
+        this.calendarTitle.innerText = "Année " + this.year;
+        this.initGrid();
     });
 }
 
+/**
+ * Affiche les dates du mois qui contiennent des données (petits ronds)
+ * @param {int} month Mois dont on veut afficher l'indication des données
+ * @param {int} year Année du mois dont on veut afficher l'indication des données
+ */
 Calendar.prototype.showDatesOfMonth = function(month, year) {
     const url = "calendarScript.php?currentMonth="+(month+1)+"&currentYear="+year;
     let objCalendar = this;
@@ -218,6 +254,10 @@ Calendar.prototype.showDatesOfMonth = function(month, year) {
     });
 }
 
+/**
+ * Selectionne et affiche les données de l'année en paramètre
+ * @param {int} dateParam date dont on veut récupérer ses données
+ */
 Calendar.prototype.getInfoDate = function(dateParam) {
     const url = "getDetailsDate.php?currentDay="+dateParam.getDate()+"&currentMonth="+(dateParam.getMonth()+1)+"&currentYear="+dateParam.getFullYear();
     let objCalendar = this;
@@ -226,8 +266,6 @@ Calendar.prototype.getInfoDate = function(dateParam) {
     fetch(url).then(function (response) { //Ensuite on récupère les données de la bdd
         return response.json();
     }).then(function(body) { //Ensuite on afficher le résultat
-        console.log("getInfoDate : ");
-        console.log(body);
 
         //On réinitialise les objets déjà affichés
         objCalendar.calendarDetailsContainer.innerHTML = "";
@@ -238,8 +276,9 @@ Calendar.prototype.getInfoDate = function(dateParam) {
         dateTitle.innerText += dateParam.getDate() + " " + objCalendar.listMonths[dateParam.getMonth()] + " " + dateParam.getFullYear();
         objCalendar.calendarDetailsContainer.appendChild(dateTitle);
 
-        if (body.length != 0) {
-            //Pour chaque don trouvé
+        //Si on a trouvé au moins une donnée
+        if (body.length > 0) {
+            //Pour chaque don trouvé on crée l'html pour afficher le don
             Array.from(body).forEach(elt => {
                 let detailsContainer = document.createElement("div");
 
@@ -263,6 +302,7 @@ Calendar.prototype.getInfoDate = function(dateParam) {
                 objCalendar.calendarDetailsContainer.appendChild(detailsContainer);
             });
         }
+        //Sinon si on ne trouve rien on affiche à l'utilisateur que l'on a rien trouvé
         else {
             let detailsContainer = document.createElement("div");
 
