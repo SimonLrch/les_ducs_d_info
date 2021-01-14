cd2f455dc67fd8c954faf1d61ea3d86873cd8561
Thibault <55053185+Spaicat@users.noreply.github.com>
2021-01-14 22:31:50 +0100

    Esthetisme des mentions

--- Site/MentionLegale/CGU.php
+++ Site/MentionLegale/CGU.php
@@ -1,106 +1,108 @@
 <!DOCTYPE html>
 <html lang="fr">
-	<head>
-		<meta charset="utf-8">
-		<title>Accueil</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="../style/mainStyle.css">
-		<script defer src="script/mainScript.js"></script>
-	</head>
-	<body>
+<head>
+	<meta charset="utf-8">
+	<title>Accueil</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../style/mainStyle.css">
+	<script defer src="script/mainScript.js"></script>
+</head>
+<body>
 	<?php include_once('../include/mainHeader.php')?>
-<h1 style="text-align:center">Conditions générales d'utilisation</h1> 
+	<main class="container-main">
+		<section class="inner-box section-hero">
+			<span class="titreSection">Conditions générales d'utilisation</span>
+		</section>
+		<section class="inner-box mention">
+				<p><i>En vigueur au 01/01/2021</i></p>
 
-        <p style="text-align:center"><i>En vigueur au 01/01/2021</i></p> 
+				<p>Les présentes conditions générales d'utilisation (dites « <strong>CGU</strong> ») ont pour objet l'encadrement juridique des modalités de mise à disposition du site et des services par Sayens et de définir les conditions d’accès et d’utilisation des services par « <strong>l'Utilisateur</strong> ».<br/>Les présentes CGU sont accessibles sur le site à la rubrique «<strong>CGU</strong>».</p>
 
-        <p style="text-align:justify">Les présentes conditions générales d'utilisation (dites « <strong>CGU</strong> ») ont pour objet l'encadrement juridique des modalités de mise à disposition du site et des services par Sayens et de définir les conditions d’accès et d’utilisation des services par « <strong>l'Utilisateur</strong> ».<br/>Les présentes CGU sont accessibles sur le site à la rubrique «<strong>CGU</strong>».</p>
+				<p>Toute inscription ou utilisation du site implique l'acceptation sans aucune réserve ni restriction des présentes CGU par l’utilisateur. Lors de l'inscription sur le site via le Formulaire d’inscription, chaque utilisateur accepte expressément les présentes CGU en cochant la case précédant le texte suivant : <br/>
+				<i>« Je reconnais avoir lu et compris les CGU et je les accepte ».</i><br/>
+				<br/>
+				En cas de non-acceptation des CGU stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès des services proposés par le site.<br/>
+				www.urldusite.fr se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes CGU.</p>
 
-        <p style="text-align:justify">Toute inscription ou utilisation du site implique l'acceptation sans aucune réserve ni restriction des présentes CGU par l’utilisateur. Lors de l'inscription sur le site via le Formulaire d’inscription, chaque utilisateur accepte expressément les présentes CGU en cochant la case précédant le texte suivant : <br/>
-        <i>« Je reconnais avoir lu et compris les CGU et je les accepte ».</i><br/>
-        <br/>
-        En cas de non-acceptation des CGU stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès des services proposés par le site.<br/>
-        www.urldusite.fr se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes CGU.</p>
+				<h2>Article 1 : Les mentions légales</h2>
 
-    <h2>Article 1 : Les mentions légales</h2>
+				<p>L’édition et la direction de la publication du site www.urldusite.fr est assurée par Sayens, domicilié 64 rue de Sully, 21000 Dijon.<br/>
+				Numéro de téléphone : non indiqué<br/>
+				Adresse e-mail : cnicolle@u-bourgogne.fr</p>
 
-        <p style="text-align:justify">L’édition et la direction de la publication du site www.urldusite.fr est assurée par Sayens, domicilié 64 rue de Sully, 21000 Dijon.<br/>
-        Numéro de téléphone : non indiqué<br/>
-        Adresse e-mail : cnicolle@u-bourgogne.fr</p>
+				<p>La personne est assujetie au RCS avec le numéro d'inscription __________ et son numéro de TVA intracommunautaire est le ____________</p>
 
-        <p style="text-align:justify">La personne est assujetie au RCS avec le numéro d'inscription __________ et son numéro de TVA intracommunautaire est le ____________</p>
+				<p>L'hébergeur du site www.urldusite.fr est la société Sayens, dont le siège social est situé au 64 rue de Sully, 21000 Dijon, avec le numéro de téléphone : Non indiqué.</p>
 
-        <p style="text-align:justify">L'hébergeur du site www.urldusite.fr est la société Sayens, dont le siège social est situé au 64 rue de Sully, 21000 Dijon, avec le numéro de téléphone : Non indiqué.</p>
+			<h2>Article 2 : Accès au site</h2>
 
-    <h2>Article 2 : Accès au site</h2>
+				<p>Le site www.urldusite.fr permet à l'Utilisateur un accès gratuit aux services suivants :</p>
 
-        <p style="text-align:justify">Le site www.urldusite.fr permet à l'Utilisateur un accès gratuit aux services suivants :</p>
+				<p>Le site internet propose les services suivants :<br/>
+				</p>
 
-        <p style="text-align:justify">Le site internet propose les services suivants :<br/>
-        </p>
+				<p>Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet. Tous les frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion Internet, etc.) sont à sa charge.</p>
 
-        <p style="text-align:justify">Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet. Tous les frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion Internet, etc.) sont à sa charge.</p>
+				<p>L’Utilisateur non membre n'a pas accès aux services réservés. Pour cela, il doit s’inscrire en remplissant le formulaire. En acceptant de s’inscrire aux services réservés, l’Utilisateur membre s’engage à fournir des informations sincères et exactes concernant son état civil et ses coordonnées, notamment son adresse email.<br/>
+				Pour accéder aux services, l’Utilisateur doit ensuite s'identifier à l'aide de son identifiant et de son mot de passe qui lui seront communiqués après son inscription.<br/>
+				Tout Utilisateur membre régulièrement inscrit pourra également solliciter sa désinscription en se rendant à la page dédiée sur son espace personnel. Celle-ci sera effective dans un délai raisonnable.<br/>
+				Tout événement dû à un cas de force majeure ayant pour conséquence un dysfonctionnement du site ou serveur et sous réserve de toute interruption ou modification en cas de maintenance, n'engage pas la responsabilité de www.urldusite.fr. Dans ces cas, l’Utilisateur accepte ainsi ne pas tenir rigueur à l’éditeur de toute interruption ou suspension de service, même sans préavis.<br/>
+				L'Utilisateur a la possibilité de contacter le site par messagerie électronique à l’adresse email de l’éditeur communiqué à l’ARTICLE 1.</p>
 
-        <p style="text-align:justify">L’Utilisateur non membre n'a pas accès aux services réservés. Pour cela, il doit s’inscrire en remplissant le formulaire. En acceptant de s’inscrire aux services réservés, l’Utilisateur membre s’engage à fournir des informations sincères et exactes concernant son état civil et ses coordonnées, notamment son adresse email.<br/>
-        Pour accéder aux services, l’Utilisateur doit ensuite s'identifier à l'aide de son identifiant et de son mot de passe qui lui seront communiqués après son inscription.<br/>
-        Tout Utilisateur membre régulièrement inscrit pourra également solliciter sa désinscription en se rendant à la page dédiée sur son espace personnel. Celle-ci sera effective dans un délai raisonnable.<br/>
-        Tout événement dû à un cas de force majeure ayant pour conséquence un dysfonctionnement du site ou serveur et sous réserve de toute interruption ou modification en cas de maintenance, n'engage pas la responsabilité de www.urldusite.fr. Dans ces cas, l’Utilisateur accepte ainsi ne pas tenir rigueur à l’éditeur de toute interruption ou suspension de service, même sans préavis.<br/>
-    L'Utilisateur a la possibilité de contacter le site par messagerie électronique à l’adresse email de l’éditeur communiqué à l’ARTICLE 1.</p>
+			<h2>Article 3 : Collecte des données</h2>
 
-    <h2>Article 3 : Collecte des données</h2>
+				<p>Le site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés. Le site est déclaré à la CNIL sous le numéro Non rensigné.<br/>
+				En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit :</p>
+				<ul>
+					<li>par mail à l'adresse mail support.nondusite@gmail.com</li>
+					<li>via son espace personnel </li>
+				</ul>
 
-        <p style="text-align:justify">Le site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés. Le site est déclaré à la CNIL sous le numéro Non rensigné.<br/>
-        En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit :
+			<h2>Article 4 : Propriété intellectuelle</h2>
 
-            <ul>
-                <li>par mail à l'adresse mail support.nondusite@gmail.com</li>
-                <li>via son espace personnel </li>
-            </ul>
-        </p>
-
-    <h2>Article 4 : Propriété intellectuelle</h2>
-
-        <p style="text-align:justify">Les marques, logos, signes ainsi que tous les contenus du site (textes, images, son…) font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.</p>
-
-
-        <p style="text-align:justify">L'Utilisateur doit solliciter l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus. Il s'engage à une utilisation des contenus du site dans un cadre strictement privé, toute utilisation à des fins commerciales et publicitaires est strictement interdite.<br/>
-        Toute représentation totale ou partielle de ce site par quelque procédé que ce soit, sans l’autorisation expresse de l’exploitant du site Internet constituerait une contrefaçon sanctionnée par l’article L 335-2 et suivants du Code de la propriété intellectuelle.<br/>
-        Il est rappelé conformément à l’article L122-5 du Code de propriété intellectuelle que l’Utilisateur qui reproduit, copie ou publie le contenu protégé doit citer l’auteur et sa source.</p>
-
-    <h2>Article 5 : Responsabilité</h2>
-    
-        <p style="text-align:justify">Les sources des informations diffusées sur le site www.urldusite.fr sont réputées fiables mais le site ne garantit pas qu’il soit exempt de défauts, d’erreurs ou d’omissions.<br/>
-        Les informations communiquées sont présentées à titre indicatif et général sans valeur contractuelle. Malgré des mises à jour régulières, le site www.urldusite.fr ne peut être tenu responsable de la modification des dispositions administratives et juridiques survenant après la publication. De même, le site ne peut être tenue responsable de l’utilisation et de l’interprétation de l’information contenue dans ce site.</p>
+				<p>Les marques, logos, signes ainsi que tous les contenus du site (textes, images, son…) font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.</p>
 
 
-        <p style="text-align:justify">L'Utilisateur s'assure de garder son mot de passe secret. Toute divulgation du mot de passe, quelle que soit sa forme, est interdite. Il assume les risques liés à l'utilisation de son identifiant et mot de passe. Le site décline toute responsabilité.</p>
+				<p>L'Utilisateur doit solliciter l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus. Il s'engage à une utilisation des contenus du site dans un cadre strictement privé, toute utilisation à des fins commerciales et publicitaires est strictement interdite.<br/>
+				Toute représentation totale ou partielle de ce site par quelque procédé que ce soit, sans l’autorisation expresse de l’exploitant du site Internet constituerait une contrefaçon sanctionnée par l’article L 335-2 et suivants du Code de la propriété intellectuelle.<br/>
+				Il est rappelé conformément à l’article L122-5 du Code de propriété intellectuelle que l’Utilisateur qui reproduit, copie ou publie le contenu protégé doit citer l’auteur et sa source.</p>
+
+			<h2>Article 5 : Responsabilité</h2>
+
+				<p>Les sources des informations diffusées sur le site www.urldusite.fr sont réputées fiables mais le site ne garantit pas qu’il soit exempt de défauts, d’erreurs ou d’omissions.<br/>
+				Les informations communiquées sont présentées à titre indicatif et général sans valeur contractuelle. Malgré des mises à jour régulières, le site www.urldusite.fr ne peut être tenu responsable de la modification des dispositions administratives et juridiques survenant après la publication. De même, le site ne peut être tenue responsable de l’utilisation et de l’interprétation de l’information contenue dans ce site.</p>
 
 
-        <p style="text-align:justify">Le site www.urldusite.fr ne peut être tenu pour responsable d’éventuels virus qui pourraient infecter l’ordinateur ou tout matériel informatique de l’Internaute, suite à une utilisation, à l’accès, ou au téléchargement provenant de ce site.<br/>
-        La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.</p>
-
-    <h2>Article 6 : Liens hypertextes</h2>
-
-        <p style="text-align:justify">Des liens hypertextes peuvent être présents sur le site. L’Utilisateur est informé qu’en cliquant sur ces liens, il sortira du site www.urldusite.fr. Ce dernier n’a pas de contrôle sur les pages web sur lesquelles aboutissent ces liens et ne saurait, en aucun cas, être responsable de leur contenu.</p>
-
-    <h2>Article 7 : Cookies</h2>
-
-        <p style="text-align:justify">L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement sur son logiciel de navigation.</p>
-        <p style="text-align:justify">Les cookies sont de petits fichiers stockés temporairement sur le disque dur de l’ordinateur de l’Utilisateur par votre navigateur et qui sont nécessaires à l’utilisation du site www.urldusite.fr. Les cookies ne contiennent pas d’information personnelle et ne peuvent pas être utilisés pour identifier quelqu’un. Un cookie contient un identifiant unique, généré aléatoirement et donc anonyme. Certains cookies expirent à la fin de la visite de l’Utilisateur, d’autres restent.
-        L’information contenue dans les cookies est utilisée pour améliorer le site www.urldusite.fr.
-        En naviguant sur le site, L’Utilisateur les accepte.</p>
+				<p>L'Utilisateur s'assure de garder son mot de passe secret. Toute divulgation du mot de passe, quelle que soit sa forme, est interdite. Il assume les risques liés à l'utilisation de son identifiant et mot de passe. Le site décline toute responsabilité.</p>
 
 
-        <p style="text-align:justify">L’Utilisateur doit toutefois donner son consentement quant à l’utilisation de certains cookies.
-        A défaut d’acceptation, l’Utilisateur est informé que certaines fonctionnalités ou pages risquent de lui être refusées.</p>
+				<p>Le site www.urldusite.fr ne peut être tenu pour responsable d’éventuels virus qui pourraient infecter l’ordinateur ou tout matériel informatique de l’Internaute, suite à une utilisation, à l’accès, ou au téléchargement provenant de ce site.<br/>
+				La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.</p>
+
+			<h2>Article 6 : Liens hypertextes</h2>
+
+				<p>Des liens hypertextes peuvent être présents sur le site. L’Utilisateur est informé qu’en cliquant sur ces liens, il sortira du site www.urldusite.fr. Ce dernier n’a pas de contrôle sur les pages web sur lesquelles aboutissent ces liens et ne saurait, en aucun cas, être responsable de leur contenu.</p>
+
+			<h2>Article 7 : Cookies</h2>
+
+				<p>L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement sur son logiciel de navigation.</p>
+				<p>Les cookies sont de petits fichiers stockés temporairement sur le disque dur de l’ordinateur de l’Utilisateur par votre navigateur et qui sont nécessaires à l’utilisation du site www.urldusite.fr. Les cookies ne contiennent pas d’information personnelle et ne peuvent pas être utilisés pour identifier quelqu’un. Un cookie contient un identifiant unique, généré aléatoirement et donc anonyme. Certains cookies expirent à la fin de la visite de l’Utilisateur, d’autres restent.
+				L’information contenue dans les cookies est utilisée pour améliorer le site www.urldusite.fr.
+				En naviguant sur le site, L’Utilisateur les accepte.</p>
 
 
-        <p style="text-align:justify">L’Utilisateur pourra désactiver ces cookies par l’intermédiaire des paramètres figurant au sein de son logiciel de navigation.</p>
+				<p>L’Utilisateur doit toutefois donner son consentement quant à l’utilisation de certains cookies.
+				A défaut d’acceptation, l’Utilisateur est informé que certaines fonctionnalités ou pages risquent de lui être refusées.</p>
 
-    <h2>Article 8 : Droit applicable et juridiction compétente</h2>
 
-        <p style="text-align:justify">La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un litige né entre les parties, les tribunaux français seront seuls compétents pour en connaître.
-        Pour toute question relative à l’application des présentes CGU, vous pouvez joindre l’éditeur aux coordonnées inscrites à l’ARTICLE 1.</p>
+				<p>L’Utilisateur pourra désactiver ces cookies par l’intermédiaire des paramètres figurant au sein de son logiciel de navigation.</p>
 
-        <?php include_once('../include/mainFooter.php')?>
-	</body>
+			<h2>Article 8 : Droit applicable et juridiction compétente</h2>
+
+				<p>La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un litige né entre les parties, les tribunaux français seront seuls compétents pour en connaître.
+				Pour toute question relative à l’application des présentes CGU, vous pouvez joindre l’éditeur aux coordonnées inscrites à l’ARTICLE 1.</p>
+		</section>
+	</main>
+	<?php include_once('../include/mainFooter.php')?>
+</body>
 <html>
